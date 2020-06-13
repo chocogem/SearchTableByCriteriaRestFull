1f3c620cf9723d2d254e720f7a6d9a813509a5d2
Prapassorn <uprapassorn@gmail.com>
2020-06-13 19:08:55 +0700

    Modified Query

--- SearchMovieApplication/src/main/java/com/demo/movie/repositories/MovieRepository.java
+++ SearchMovieApplication/src/main/java/com/demo/movie/repositories/MovieRepository.java
@@ -8,10 +8,13 @@ import org.springframework.data.mongodb.repository.Query;
 import com.demo.movie.entities.Movie;
 
 public interface MovieRepository extends MongoRepository<Movie, String>{
-	@Query("{ 'title' :{ $regex :?0 , $options: 'i'} }")
+	@Query(value="{'title' : { $regex:?0 , $options: 'i'}}",sort="{ 'movieId': 1 }")
     List<Movie> searchMoviesByTitle(String keyword);
 	
-	@Query("{'genres' :{ $in :?0 }}")
-	List<Movie> searchMoviesByGenre(String[] keyword);
+	@Query(value="{'genres' :{ $in :[{$regex:?0 , $options: 'i' }] }}",sort="{ 'movieId': 1 }")
+	List<Movie> searchMoviesByGenre(String keyword);
 	//List<Movie> findByGenresIn(String[] tags);
+	
+	
+	//List<Movie>findByTitleContaining(String keyword);
 }
